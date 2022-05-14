57e315abf062b36dac08743f3e7d9e7d2d6d2ff2
petar2518 <pm20180025@student.fon.bg.ac.rs>
2022-05-14 15:07:21 +0200

    ispravljena stamparska greska

--- config/konekcija.php
+++ config/konekcija.php
@@ -7,7 +7,7 @@ $mysqli =new mysqli($mysql_server, $mysql_user, $mysql_password, $mysql_db);
 
 
 if ($mysqli->connect_errno){
-    exit("Nauspesna konekcija: greska> ".$mysqli->connect_error.", err kod>".$mysqli->connect_errno);
+    exit("Neuspesna konekcija: greska> ".$mysqli->connect_error.", err kod>".$mysqli->connect_errno);
 }
 
 ?>
