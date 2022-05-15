e27854d991b49862f3a97ba7f2f6f301330de871
petar2518 <pm20180025@student.fon.bg.ac.rs>
2022-05-15 17:19:43 +0200

    Dodati linkovi ka socijalnim mrezama

--- design/footer.php
+++ design/footer.php
@@ -1,6 +1,17 @@
 <!-- HTML FOOTER -->
 <footer class="section">
-        <img src="images/logo.jpg" class="logo">
-        <div class="center purple-text"> KartKarting 2022 &copy;</div>
+        <img src="images/logo.jpg" class="logo"><br>
+        <p style="text-align:center;">
+            <a href="https://www.facebook.com/Formula1/"><img width="40" height="40" src="images/fb.png"></a>
+            <a href="https://www.instagram.com/f1/"><img width="40" height="40" src="images/ig.jpg"></i></a>
+            <a href="https://www.youtube.com/user/Formula1"><img width="40" height="40" src="images/yt.png"></i></a>
+            <div class="center purple-text"> KartKarting 2022 &copy;
+</p>
+        </div>
+       
+        <div>
+           
+        </div>
+        
     </footer>
 </body>
