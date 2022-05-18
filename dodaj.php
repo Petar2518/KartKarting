eb4b635467040d055222cd9405dc4a9ba6ab6580
petar2518 <pm20180025@student.fon.bg.ac.rs>
2022-05-18 10:21:41 +0200

    Dodata funkcionalna strana za dodavanje termina

---
+++ dodaj.php
@@ -0,0 +1,19 @@
+<?php
+include 'config/konekcija.php';
+include 'domen/sport.php';
+include 'domen/trajanje.php';
+include 'domen/termin.php';
+
+  $korisnik=trim($_POST['username']);
+  $rezervisao = trim($_POST['rezervisao']);
+  $sport = trim($_POST['sport']);
+  $trajanje = trim($_POST['trajanje']);
+  $datumRezervacije = trim($_POST['datumRezervacije']);
+  $cena = trim($_POST['cena']);
+
+  Termin::sacuvaj($mysqli,$sport,$trajanje,$rezervisao,$datumRezervacije,$cena);
+
+  header("Location: pocetna.php?user=".$korisnik);
+
+
+ ?>
